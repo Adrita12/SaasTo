f5409f598f4d557b93d3d75acffaafea47c9ae7f
Adrita Bhattacharjee <98798926+Adrita12@users.noreply.github.com>
2024-06-22 01:43:05 +0530

    4

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+const burger = document.querySelector('.burger');
+const nav = document.getElementById('nav');
+const segment2 = document.querySelector('.segment2');
+const segment3 = document.getElementById('segment3');
 
-    burger.addEventListener('click',()=>{
-        nav.classList.toggle('nav-active');
-    });
-}
-
-navSlide();
+burger.addEventListener('click', () => {
+    nav.classList.toggle('side-nav-active');
+    segment2.classList.toggle('side-nav');
+    segment3.classList.toggle('side-nav');
+});
